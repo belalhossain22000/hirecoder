b9aaa534534edca5a923c7a6cf9b912da2b07a4a
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-10-20 23:59:28 +0600

    added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
+
+const Main = () => {
+    return (
+
+        <>
+        <Navbar></Navbar>
+            <Outlet></Outlet>
+        <Footer></Footer>
+        </>
+
+
+    );
+};
+
+export default Main;
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
+import Skills from "../Pages/Steps/Skills/Skills";
+import Education from "../Pages/Steps/Education/Education";
+// import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Login></Login>,
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: 'skills',
+        element: <Skills></Skills>
+      }
+      ,
+      {
+        path: "education",
+        element: <Education></Education>
+      }
+    ],
+  },
+  {
+    path:"/stepper",
+    element:<Footer/>
+  }
+]);
